9272623a1525cd4010e3712fac64ecb3fcf6910a
Ольга Скворцова <olgaskvorcova@MacBook-Air-Olga.local>
2024-01-13 20:58:43 +0300

    finished project

--- src/components/taskList/taskList.js
+++ src/components/taskList/taskList.js
@@ -1,22 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './taskList.css';
+import PropTypes from 'prop-types';
 import Task from '../task';
 
-function TaskList(props) {
-  const [renderedTasks, setRenderedTasks] = useState([]); /* создается renderedTasks, в котором пусой массив и setRenderedTasks, который изменяет этот массив */
-
-  useEffect(() => {
-    const tasksComponents = props.allTasks.map((el, index) => (/* создается массив tasksComponents, который проходит по массиву allTasks, получает оттуда task и индекс task. */
-      <Task key={index} allTasks={el} /> /* создается элемент Task, в который записывается ключ и в который передается task */
-    ));
-    setRenderedTasks(tasksComponents);
-  }, [props.allTasks]);
+function TaskList({
+  todos, destroyItem, changeItem, onToggleDone,
+}) {
+  const elements = todos.map(({ id, ...anotherProps }) => (
+    <li key={id}>
+      <Task
+        /* eslint-disable react/jsx-props-no-spreading */
+        {...anotherProps}
+        destroyItem={() => destroyItem(id)}
+        changeItem={(label) => changeItem(id, label)}
+        onToggleDone={() => onToggleDone(id)}
+        id={id}
+      />
+    </li>
+  ));
 
   return (
     <ul className="todo-list">
-      {renderedTasks}
+      {elements}
     </ul>
   );
 }
+
+TaskList.defaultProps = {
+  todos: [],
+  destroyItem: () => {},
+  changeItem: () => {},
+  onToggleDone: () => {},
+};
+
+TaskList.propTypes = {
+  todos: PropTypes.instanceOf(Array),
+  destroyItem: PropTypes.func,
+  changeItem: PropTypes.func,
+  onToggleDone: PropTypes.func,
+};
 
 export default TaskList;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from './components/app';
 
+// eslint-disable-next-line no-undef
 const root = document.getElementById('root');
 const rootContainer = createRoot(root);
 
